cd2eb0381802187acd0bd09bbdce14d8b58c1f6c
khalil <khalillakhdharatc@gmail.com>
2023-02-16 19:21:33 +0100

    ap

--- src/App.js
+++ src/App.js
@@ -1,79 +1,81 @@
-import './App.css';
-import "bootstrap/dist/css/bootstrap.css";
+import React from 'react'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 import CreateStudent from './components/create-student'
 import EditStudent from './components/edit-student'
 import StudentList from './components/student-list'
+
 function App() {
   return (
     <div className="App">
-    <Router>
-      <header className="App-header">
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand>
-              <Link to={'/create-student'} className="nav-link">
-                React MERN Stack App
-              </Link>
-            </Navbar.Brand>
-
-            <Nav className="justify-content-end">
-              <Nav>
+      <Router>
+        <header className="App-header">
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand>
                 <Link to={'/create-student'} className="nav-link">
-                  Create Student
+                  React MERN Stack App
                 </Link>
-              </Nav>
+              </Navbar.Brand>
 
-              <Nav>
-                <Link to={'/student-list'} className="nav-link">
-                  Student List
-                </Link>
-              </Nav>
-            </Nav>
-          </Container>
-        </Navbar>
-      </header>
+              <Nav className="justify-content-end">
+                <Nav>
+                  <Link to={'/create-student'} className="nav-link">
+                    Create Student
+                  </Link>
+                </Nav>
 
-      <Container>
-        <Row>
-          <Col md={12}>
-            <div className="wrapper">
-              <Switch>
-                <Route
-                  exact
-                  path="/"
-                  component={(props) => <CreateStudent {...props} />}
-                />
-                <Route
-                  exact
-                  path="/create-student"
-                  component={(props) => <CreateStudent {...props} />}
-                />
-                <Route
-                  exact
-                  path="/edit-student/:id"
-                  component={(props) => <EditStudent {...props} />}
-                />
-                <Route
-                  exact
-                  path="/student-list"
-                  component={(props) => <StudentList {...props} />}
-                />
-              </Switch>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </Router>
-  </div>
-  );
+                <Nav>
+                  <Link to={'/student-list'} className="nav-link">
+                    Student List
+                  </Link>
+                </Nav>
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
+
+        <Container>
+          <Row>
+            <Col md={12}>
+              <div className="wrapper">
+                <Switch>
+                  <Route
+                    exact
+                    path="/"
+                    component={(props) => <CreateStudent {...props} />}
+                  />
+                  <Route
+                    exact
+                    path="/create-student"
+                    component={(props) => <CreateStudent {...props} />}
+                  />
+                  <Route
+                    exact
+                    path="/edit-student/:id"
+                    component={(props) => <EditStudent {...props} />}
+                  />
+                  <Route
+                    exact
+                    path="/student-list"
+                    component={(props) => <StudentList {...props} />}
+                  />
+                </Switch>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Router>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/create-student.js
+++ src/components/create-student.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
-
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
 export default class CreateStudent extends Component {
+  constructor(props)
+  {
+    super(props);
+    // eslint-disable-next-line no-this-before-super
+    this.state={
+      name:'',
+      email:'',
+      rollno:'',
+    }
+  }
+  onChange= e=>{
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
   render() {
     return (
-      <>
-        <h1>CreateStudent works</h1>
-      </>
+      <div className="form-wrapper">
+        <Form>
+          <Form.Group controlId="name">
+            <Form.Label>Nom:</Form.Label>
+            <Form.Control type="text" value={this.state.name}  name="name" onChange={this.onChange}></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="email">
+            <Form.Label>Email:</Form.Label>
+            <Form.Control type="text" value={this.state.email} name="email" onChange={this.onChange}></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="rollno">
+            <Form.Label>numéro:</Form.Label>
+            <Form.Control type="text" value={this.state.rollno} name="rollno" onChange={this.onChange}></Form.Control>
+          </Form.Group>
+          <Button variant="danger" size="lg" block="block" type="submit" className="mt-4">
+          Create Student
+        </Button>
+        </Form>
+        
+      </div>
     )
   }
 }
